4dcc517d4fdd179579206aa72ffc08056ba6367c
Fadl-Matar <s195846@student.dtu.dk>
2026-01-09 11:47:42 +0100

    Updated the build and test to include the jar file

--- build_and_test.sh
+++ build_and_test.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 set -e
 chmod +x mvnw
-./mvnw compile quarkus:dev
+./mvnw compile 
+mvn package -Dmaven.test.skip
+java -jar target/quarkus-app/quarkus-run.jar &
 QUARKUS_PID=$!
 ./mvnw -q test
 echo "Stopping Quarkus application with PID $QUARKUS_PID"
