04cd48a1857e26a4401cfc517105ee17289e788c
yayuxi <eliasyanmortensen@gmail.com>
2026-01-13 15:40:22 +0100

    accountSteps imports

--- dtu-pay-E2ETest/src/test/java/dtu/pay/AccountSteps.java
+++ dtu-pay-E2ETest/src/test/java/dtu/pay/AccountSteps.java
@@ -1,20 +1,45 @@
-@Given("a user with name {string}, last name {string}, and CPR {string}")
-public void aUserWithNameLastNameAndCPR(String string, String string2, String string3) {
-    // Write code here that turns the phrase above into concrete actions
-    throw new io.cucumber.java.PendingException();
-}
-@When("the user registers for a DTU Pay account")
-public void theUserRegistersForADTUPayAccount() {
-    // Write code here that turns the phrase above into concrete actions
-    throw new io.cucumber.java.PendingException();
-}
-@Then("the account creation is successful")
-public void theAccountCreationIsSuccessful() {
-    // Write code here that turns the phrase above into concrete actions
-    throw new io.cucumber.java.PendingException();
-}
-@Then("the user has a DTU Pay account associated with their bank account")
-public void theUserHasADTUPayAccountAssociatedWithTheirBankAccount() {
-    // Write code here that turns the phrase above into concrete actions
-    throw new io.cucumber.java.PendingException();
+package dtu.pay;
+
+import dtu.ws.fastmoney.Account;
+import dtu.ws.fastmoney.BankService;
+import dtu.ws.fastmoney.BankServiceException_Exception;
+import dtu.ws.fastmoney.BankService_Service;
+import dtu.ws.fastmoney.User;
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+public class AccountSteps {
+    @Given("a user with name {string}, last name {string}, and CPR {string}")
+    public void aUserWithNameLastNameAndCPR(String string, String string2, String string3) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @When("the user registers for a DTU Pay account")
+    public void theUserRegistersForADTUPayAccount() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("the account creation is successful")
+    public void theAccountCreationIsSuccessful() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("the user has a DTU Pay account associated with their bank account")
+    public void theUserHasADTUPayAccountAssociatedWithTheirBankAccount() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
 }
