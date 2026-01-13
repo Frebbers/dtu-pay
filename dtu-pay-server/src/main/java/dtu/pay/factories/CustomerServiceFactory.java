8d97d2d4b92256576eaa54e38018a17db23bc2eb
Frederik Bode Hendrichsen <fbhendrich@lightera.com>
2026-01-13 17:04:30 +0100

    imported utilities from huba

--- dtu-pay-server/src/main/java/dtu/pay/factories/CustomerServiceFactory.java
+++ dtu-pay-server/src/main/java/dtu/pay/factories/CustomerServiceFactory.java
@@ -1,8 +1,12 @@
 package dtu.pay.factories;
 
 import dtu.pay.services.CustomerService;
+import messaging.implementations.RabbitMqQueue;
 
 public class CustomerServiceFactory {
+    public CustomerServiceFactory() {
+        var mq = new RabbitMqQueue("customerServiceQueue");
+    }
     public CustomerService getService() {
         return new CustomerService();
     }
