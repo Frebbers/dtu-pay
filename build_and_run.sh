c7e5175e38567b7543e72e64be1289df913cf460
Fadl-Matar <s195846@student.dtu.dk>
2026-01-13 14:11:38 +0100

    Added the build script from Huberts example

---
+++ build_and_run.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+set -e
+
+pushd dtu-pay-server
+mvn clean package
+# Create a new docker image if necessary.
+docker compose build
+# Restarts the container with the new image if necessary
+docker compose up -d
+# The server stays running.
+# To terminate the server run docker-compose down in the
+# simple-rest directory
+# clean up images
+docker image prune -f 
+popd
+
+# Give the Web server a chance to finish start up
+sleep 2 
+
+pushd dtu-pay-client 
+mvn clean test
+popd
