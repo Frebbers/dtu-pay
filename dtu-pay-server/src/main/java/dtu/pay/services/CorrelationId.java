883012b1574cb8efbf58cbf462ad1151879c072f
Mattia Zanellato <mattia.zanellato00@gmail.com>
2026-01-14 10:20:55 +0100

    first partial implementation of CustomerService

---
+++ dtu-pay-server/src/main/java/dtu/pay/services/CorrelationId.java
@@ -0,0 +1,9 @@
+package dtu.pay.services;
+
+import java.util.UUID;
+
+public record CorrelationId(UUID id) {
+    public static CorrelationId randomId() {
+        return new CorrelationId(UUID.randomUUID());
+    }
+}
---
+++ dtu-pay-server/src/test/java/dtu/pay/services/CustomerServiceTest.java
@@ -0,0 +1,26 @@
+package dtu.pay.services;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CustomerServiceTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void register() {
+    }
+
+    @Test
+    void handleCustomerRegistered() {
+    }
+}
