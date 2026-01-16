0d5b66bcbd65cf05fa4d68592fe0917eb1a9d07b
Frederik Bode Hendrichsen <fbhendrich@lightera.com>
2026-01-16 20:04:27 +0100

    fixed build and test script

--- build_and_test.sh
+++ build_and_test.sh
@@ -1,25 +1,16 @@
 #!/bin/bash
 set -e
-#TODO update this to run all tests
-echo "Building Messaging Utilities..."
-mvn clean package -f utilities/messaging-utilities/pom.xml
+echo "Stopping containers..."
+docker compose down
 
-echo "Building DTU Pay Server..."
-#sh dtu-pay-server/build.sh
-mvn clean package -f dtu-pay-server/pom.xml
-docker compose -f dtu-pay-server/compose.yml build
+echo "Building project..."
+mvn package -DskipTests
 
-#echo "Running server tests..."
-#mvn -q test -f dtu-pay-server/pom.xml
+echo "Starting containers using docker compose..."
+docker compose up -d --build
+sleep 10
+echo "Running tests..."
+mvn test
 
-echo "Starting Quarkus server using docker compose..."
-docker compose -f dtu-pay-server/compose.yml up -d
-
-echo "Building DTU Pay Client..."
-mvn -q compile -f dtu-pay-E2ETest/pom.xml
-
-echo "Running client tests..."
-mvn -q test -f dtu-pay-E2ETest/pom.xml
-
-echo "Stopping Quarkus container..."
-docker compose -f dtu-pay-server/compose.yml down
+echo "Stopping containers..."
+docker compose down
