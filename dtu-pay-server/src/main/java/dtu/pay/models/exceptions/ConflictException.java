ffb4778968feaf29600d30e0fc362c05f460d77a
Frederik Bode Hendrichsen <s224804@dtu.dk>
2026-01-21 10:59:07 +0100

    added a comment

--- dtu-pay-server/src/main/java/dtu/pay/models/exceptions/ConflictException.java
+++ dtu-pay-server/src/main/java/dtu/pay/models/exceptions/ConflictException.java
@@ -3,7 +3,7 @@ package dtu.pay.models.exceptions;
 import jakarta.ws.rs.ClientErrorException;
 import jakarta.ws.rs.core.Response;
 
-
+/// http code 409 - ConflictException thrown when attempting to create a resource that already exists
 public class ConflictException extends ClientErrorException {
     public ConflictException(String message) {
         super(message, Response.Status.CONFLICT);
