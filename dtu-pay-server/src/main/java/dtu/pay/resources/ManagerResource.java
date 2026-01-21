1838804b8a0df5755c9d699d89998a0d6082d350
Mattia Zanellato <mattia.zanellato00@gmail.com>
2026-01-21 22:53:32 +0100

    implemented reverted changes

--- dtu-pay-server/src/main/java/dtu/pay/resources/ManagerResource.java
+++ dtu-pay-server/src/main/java/dtu/pay/resources/ManagerResource.java
@@ -9,6 +9,7 @@ import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 
+@Path("")
 public class ManagerResource {
     private final ReportingService reportingService = new ReportingServiceFactory().getService();
 
