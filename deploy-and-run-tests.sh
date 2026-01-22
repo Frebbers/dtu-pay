1411dca1eeb639c50025658b8e355a0be3b7140b
Fadl-Matar <s195846@student.dtu.dk>
2026-01-22 13:37:15 +0100

    NEw script up and running

--- deploy-and-run-tests.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-set -e
-
-echo "Running end-to-end tests with docker-compose..."
-mvn clean docker-compose:up test -f dtu-pay-E2ETest
--- install-libraries.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-set -e
-
-echo "Installing shared libraries..."
-mvn clean install -f utilities/messaging-utilities
