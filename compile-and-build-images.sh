4d5e7c9b70a5497ec4a7ced1032b8463094da75b
Frederik Bode Hendrichsen <s224804@dtu.dk>
2026-01-22 16:47:32 +0100

    removed unused files

--- compile-and-build-images.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-set -e
-
-echo "Building Maven modules and Docker images..."
-mvn clean package docker:build -f pom.xml
--- deploy.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-set -e
-
-docker image prune -f
-docker compose up -d rabbitmq
-sleep 20
-docker compose up -d account-service token-service payment-service reporting-service dtu-pay
