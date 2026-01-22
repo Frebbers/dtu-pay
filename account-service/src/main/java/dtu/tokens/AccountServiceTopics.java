3a95e04460eb25098accd6a9e0762be54fdeca8f
Frederik Bode Hendrichsen <s224804@dtu.dk>
2026-01-22 20:57:17 +0100

    removed unused event topic "user already exists". same as user deregistration failed

--- account-service/src/main/java/dtu/tokens/AccountServiceTopics.java
+++ account-service/src/main/java/dtu/tokens/AccountServiceTopics.java
@@ -10,7 +10,6 @@ public class AccountServiceTopics {
   public static final String BANK_ACCOUNT_RETRIEVED = "BankAccountRetrieved";
   public static final String BANK_ACCOUNT_RETRIEVAL_FAILED = "BankAccountRetrievalFailed";
   public static final String USER_DEREGISTRATION_FAILED = "UserDeregistrationFailed";
-  public static final String USER_DOES_NOT_EXIST = "UserDoesNotExist"; 
   public static final String USER_ALREADY_REGISTERED = "UserAlreadyRegistered";
   public static final String USER_REGISTRATION_FAILED = "UserRegistrationFailed";
 }
--- dtu-pay-server/src/main/java/dtu/pay/tokens/TokenTopics.java
+++ dtu-pay-server/src/main/java/dtu/pay/tokens/TokenTopics.java
@@ -5,8 +5,6 @@ public final class TokenTopics {
     public static final String CONSUME_TOKEN_REQUESTED = "ConsumeTokenRequested";
     public static final String TOKENS_ISSUED = "TokensIssued";
     public static final String TOKEN_REQUEST_REJECTED = "TokenRequestRejected";
-    public static final String TOKEN_CONSUMED = "TokenConsumed";
-    public static final String TOKEN_CONSUMPTION_REJECTED = "TokenConsumptionRejected";
     public static final String TOKEN_INVALIDATION_REQUESTED = "TokenInvalidationRequested";
 
     private TokenTopics() {
